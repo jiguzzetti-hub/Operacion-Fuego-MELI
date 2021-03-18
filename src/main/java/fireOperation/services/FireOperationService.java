9d763acff0ddd69ee6bc4a15d68f192234b84d5b
JGUZZETTI <JGUZZETTI@NO033982.grupobna.local>
2021-03-18 10:21:33 -0300

    Agrego estructura de respuestas y métodos de lógica

--- src/main/java/fireOperation/services/FireOperationService.java
+++ src/main/java/fireOperation/services/FireOperationService.java
@@ -1,18 +1,47 @@
 package fireOperation.services;
 
+import java.util.ArrayList;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import fireOperation.dtos.FireOperationRequest;
 import fireOperation.dtos.FireOperationResponse;
+import fireOperation.dtos.Position;
 
 @Service
 public class FireOperationService {
-	
-	public ResponseEntity<?> getMessageAndLocation (FireOperationRequest request){
+
+	public ResponseEntity<?> getMessageAndLocation(FireOperationRequest request) {
 		FireOperationResponse response = new FireOperationResponse();
-		
-		return null;
+		if ((!ValidationService.validateMessages(request.getSatellites())
+				|| (!ValidationService.validateDistances(request.getSatellites())))) {
+			return ResponseEntity.status(404).build();
+		}
+		response.setMessage(getMessage(request.getSatellites().get(0).getMessage(),
+				request.getSatellites().get(1).getMessage(), request.getSatellites().get(2).getMessage()));
+		if (response.getMessage() == null) {
+			return ResponseEntity.status(404).build();
+		}
+		response.setPosition(getLocation(request.getSatellites().get(0).getDistance(),
+				request.getSatellites().get(1).getDistance(), request.getSatellites().get(2).getDistance()));
+		if (response.getPosition() == null) {
+			return ResponseEntity.status(404).build();
+		}
+
+		return ResponseEntity.ok(response);
 	}
 
+	public String getMessage(ArrayList<String> a, ArrayList<String> b, ArrayList<String> c) {
+		String message = null;
+
+		return message;
+	}
+
+	public Position getLocation(float distanceA, float distanceB, float distanceC) {
+		Position position = null;
+
+		return position;
+
+	}
 }
