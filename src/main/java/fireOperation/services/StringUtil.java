e90e75488c516004e60e0b5dfc14d353a3184fc4
JGUZZETTI <JGUZZETTI@NO033982.grupobna.local>
2021-03-18 10:21:54 -0300

    Valido inputs

---
+++ src/main/java/fireOperation/services/StringUtil.java
@@ -0,0 +1,26 @@
+package fireOperation.services;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class StringUtil {
+	// Valido que cada palabra del mensaje sea String, considero letras y números
+	// solamente porque no aclara ningún caracter especial a considerar
+	private static final String GENDER_REGEX = "[a-zA-Z0-9]";
+
+	public static boolean validateString(ArrayList<String> messageList) {
+		for (String word : messageList) {
+			if (!containsOnlyStrings(word)) {
+				return false;
+			}
+
+		}
+		return true;
+	}
+
+	private static boolean containsOnlyStrings(String gender) {
+		return Pattern.matches(GENDER_REGEX, gender);
+
+	}
+
+}
---
+++ src/main/java/fireOperation/services/ValidationService.java
@@ -0,0 +1,31 @@
+package fireOperation.services;
+
+import java.util.ArrayList;
+
+import fireOperation.dtos.Satellite;
+
+public class ValidationService {
+	
+	//Recorro satélites y paso lista de palabras en cada mensaje a validar si son String
+	public static boolean validateMessages(ArrayList<Satellite> satellites) {
+		for (Satellite satellite : satellites) {
+
+			if (!StringUtil.validateString(satellite.getMessage())) {
+				return false;
+
+			}
+		}
+		return true;
+	}
+	
+	public static boolean validateDistances (ArrayList<Satellite> satellites) {
+		for (Satellite satellite : satellites) {
+			if (!Float.isFinite(satellite.getDistance())) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+
+}
