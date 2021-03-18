60ce3c0acdb56fc1e30130851b3e7581df3ed403
JGUZZETTI <JGUZZETTI@NO033982.grupobna.local>
2021-03-18 10:20:15 -0300

    Agrego constructores y getters y setters

--- src/main/java/fireOperation/dtos/FireOperationRequest.java
+++ src/main/java/fireOperation/dtos/FireOperationRequest.java
@@ -3,7 +3,24 @@ package fireOperation.dtos;
 import java.util.ArrayList;
 
 public class FireOperationRequest {
-	
+
 	private ArrayList<Satellite> satellites;
+
+	public ArrayList<Satellite> getSatellites() {
+		return satellites;
+	}
+
+	public void setSatellites(ArrayList<Satellite> satellites) {
+		this.satellites = satellites;
+	}
+
+	public FireOperationRequest(ArrayList<Satellite> satellites) {
+		super();
+		this.satellites = satellites;
+	}
+
+	public FireOperationRequest() {
+
+	}
 
 }
--- src/main/java/fireOperation/dtos/FireOperationResponse.java
+++ src/main/java/fireOperation/dtos/FireOperationResponse.java
@@ -3,8 +3,34 @@ package fireOperation.dtos;
 import java.util.ArrayList;
 
 public class FireOperationResponse {
-	
+
 	private Position position;
 	private String message;
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public FireOperationResponse(Position position, String message) {
+		super();
+		this.position = position;
+		this.message = message;
+	}
+
+	public FireOperationResponse() {
+
+	}
 
 }
