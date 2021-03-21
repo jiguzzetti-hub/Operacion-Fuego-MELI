d7339e1bfd3991251ba85cba06b90ee4a6cd06ab
JGUZZETTI <JGUZZETTI@NO033982.grupobna.local>
2021-03-21 06:44:03 -0300

    Valido cant de registros, nombres distintos y satelliteName =

--- src/main/java/fireOperation/services/ValidationService.java
+++ src/main/java/fireOperation/services/ValidationService.java
@@ -1,12 +1,15 @@
 package fireOperation.services;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import fireOperation.context.SatelliteNames;
 import fireOperation.dtos.Satellite;
 
 public class ValidationService {
-	
-	//Recorro satélites y paso lista de palabras en cada mensaje a validar si son String
+
+	// Recorro satélites y paso lista de palabras en cada mensaje a validar si son
+	// String
 	public static boolean validateMessages(ArrayList<Satellite> satellites) {
 		for (Satellite satellite : satellites) {
 
@@ -14,20 +17,44 @@ public class ValidationService {
 				return false;
 
 			}
-			
+
 		}
 		return true;
 	}
-	
-	public static boolean validateDistances (ArrayList<Satellite> satellites) {
+
+	public static boolean validateDistances(ArrayList<Satellite> satellites) {
 		for (Satellite satellite : satellites) {
 			if (!Float.isFinite(satellite.getDistance())) {
-				System.out.println(satellite.getDistance());
 				return false;
 			}
 		}
 		return true;
 	}
+
+	public static boolean checkMinimunOfThreeInsert(List<fireOperation.entities.Satellite> satellites) {
+		if (satellites.size() < 3) {
+			return false;
+		}
+
+		return checkSatellitesAreNotTheSame(satellites);
+	}
+
+	private static boolean checkSatellitesAreNotTheSame (List<fireOperation.entities.Satellite> satellites) {
+		if((satellites.get(0).getName().equals(satellites.get(1).getName())) || 
+				(satellites.get(1).getName().equals(satellites.get(2).getName()))){
+			return false;
+		}
+		return validateSatellitesNames(satellites);
+	}
 	
+	private static boolean validateSatellitesNames (List<fireOperation.entities.Satellite> satellites) {
+		for (fireOperation.entities.Satellite satellite : satellites) {
+			if (!satellites.contains(SatelliteNames.values())) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
 
 }
